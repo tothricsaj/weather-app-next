a51487afebd585fcce94c8d544124331af270a00
tothricsaj <tothricsaj@gmail.com>
2023-06-15 08:47:14 +0200

    feat: added style Home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,46 @@
-import Icon, { IconTypes } from "../components/Icon/Icon";
-import CityButton from "../components/City/CityButton/CityButton";
 import CityTile from "../components/City/CityTile/Citytile";
-import Button from "../components/Button/Button";
-import SearchBar from "../components/SearchBar/SearchBar";
+import classes from "../styles/Home.module.css";
 
-
-export default function Home() {
+export default function Home(props) {
+  const { responseData } = props;
+  console.log(props);
+  
   return (
-    <>
-      
-    </>
+    <div className={classes.wrapper}>
+      {
+        responseData.map(cityData => <CityTile {...cityData} key={ cityData.cityName }/>)
+      }
+    </div>
 
   );
+}
+
+export async function getStaticProps(context) {
+  const apiKey = 'ce8c93d77aad29dd1a0d877564835f66';
+  const londonLat = '51.507218';
+  const londonLon = '-0.127586';
+
+  const groupedCitiesURL = `https://api.openweathermap.org/data/2.5/group?id=524901,703448,2643743&appid=${apiKey}&units=metric`;
+
+  const res = await fetch(groupedCitiesURL);
+  const data = await res.json();
+
+  let responseData = [];
+
+  data.list.forEach((cityData) => {
+    const { name, weather, main } = cityData;
+    responseData.push({
+      cityName: name,
+      iconCode: weather[0].icon,
+      celsius: main.temp
+    });
+  });
+
+  console.log(responseData);
+  
+  return {
+    props: {
+      responseData
+    }
+  }
 }
